525256e70e5b5965196b893ddd0a7203680726fa
brunoferdias <bruno_ferdiass12@outlook.com>
2023-03-26 14:39:36 -0300

    Nome do Script atualizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:qual_e_o_endereco/insereCEP.dart';
+import 'package:qual_e_o_endereco/HomeCep.dart';
 
 
 
